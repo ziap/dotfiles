0482ba00d394dee507c6516f2276c5c38a05f687
ziap <67962871+ziap@users.noreply.github.com>
2022-08-12 13:28:21 +0700

    Add bottom border to waybar, add more zathura colors

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -8,10 +8,10 @@ cmp.setup({
     end
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ['<C-e>'] = cmp.mapping.abort()
+    ['<c-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<c-f>'] = cmp.mapping.scroll_docs(4),
+    ['<c-space>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<c-e>'] = cmp.mapping.abort()
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
