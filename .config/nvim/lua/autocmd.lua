b4a36d9a6aa2254c1058201aa09be029e4e53e25
Zap <67962871+ziap@users.noreply.github.com>
2022-09-04 17:33:14 +0700

    Add more autocmds

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,8 +1,29 @@
--- Start inserting when entering terminal buffer
-vim.api.nvim_create_autocmd(
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Start inserting after entering terminal buffer
+autocmd(
   { 'BufWinEnter', 'WinEnter' },
   {
-    pattern = { "term://*" },
-    command = "startinsert" 
+    pattern = { 'term://*' },
+    command = 'startinsert' 
   }
 )
+
+-- Highlight terminal output
+-- NOTE: don't enable yaml treesitter for this to work
+autocmd(
+  { 'TermEnter' },
+  {
+    command = 'set filetype=yaml'
+  }
+)
+
+-- Highlight yank
+autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'Visual',
+            timeout = 100,
+        })
+    end,
+})
