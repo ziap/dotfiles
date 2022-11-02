f9740f8785ba2e0c3683cef7625b11e71f95e78a
Zap <67962871+ziap@users.noreply.github.com>
2022-11-03 06:59:33 +0700

    Update neovim config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -12,7 +12,7 @@ local servers = {
 for i, name in ipairs(servers) do
   require'lspconfig'[name].setup{ 
     -- Enable completion
-    capabilities = require'cmp_nvim_lsp'.update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    capabilities = require'cmp_nvim_lsp'.default_capabilities(vim.lsp.protocol.make_client_capabilities()),
     
     -- Formatting on save
     on_attach = function(client, bufnr)
