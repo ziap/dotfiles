cde735a3116354c65f52b4146a687bf6a930b527
Zap <67962871+ziap@users.noreply.github.com>
2022-12-22 12:41:21 +0700

    Improve lualine config

--- .config/nvim/lua/plug/theme.lua
+++ .config/nvim/lua/plug/theme.lua
@@ -1,6 +1,36 @@
 vim.opt.background = 'dark'
 vim.cmd.colorscheme 'gruvbox'
 
+local filename = {
+  'filename',
+  path = 1,
+  symbols = {
+    modified = '[+]',
+    readonly = '[]',
+    unnamed = '[No Name]',
+    newfile = '[New]'
+  }
+}
+
+local function location()
+  local total = vim.api.nvim_buf_line_count(0)
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+
+  return tostring(row).."/"..total..":"..tostring(col + 1)
+end
+
+local function md_wordcount()
+  local wc = vim.fn.wordcount()
+  if vim.bo.filetype == "markdown" then
+    local words = "/"..tostring(wc.words).." words"
+    if wc.visual_words ~= nil then
+      return tostring(wc.visual_words)..words
+    end
+    return tostring(wc.cursor_words)..words
+  end
+  return ""
+end
+
 require'lualine'.setup {
   options = {
     theme = 'gruvbox',
@@ -16,17 +46,17 @@ require'lualine'.setup {
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
-    lualine_x = { 'encoding', 'fileformat' },
+    lualine_c = { filename },
+    lualine_x = { 'encoding', 'fileformat', md_wordcount },
     lualine_y = { 'filetype' },
-    lualine_z = { 'location' }
+    lualine_z = { location }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { 'filename' },
+    lualine_c = { filename },
     lualine_x = { 'filetype' },
-    lualine_y = { 'location' },
+    lualine_y = { location },
     lualine_z = {}
   }
 }
