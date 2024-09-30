fadf7acb14d0da1145cd746d9c89d9de30e4a206
Zap <67962871+ziap@users.noreply.github.com>
2024-09-30 13:39:53 +0700

    Remove greeting text

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,40 +1,18 @@
-if status is-interactive
-  # Commands to run in interactive sessions can go here
-end
+# Disable the greeting message
+set fish_greeting
 
-function fish_greeting
-  set c1 (set_color brred)
-  set c2 (set_color brgreen)
-  set c3 (set_color bryellow)
-  set c4 (set_color brblue)
-  set c5 (set_color brmagenta)
-  set c6 (set_color brcyan)
-  set c7 (set_color brwhite)
-  
-  echo "
-  $c1  ▄██████▄    $c2  ▄██████▄    $c4  ▄██████▄    $c5  ▄██████▄    $c6  ▄██████▄    
-  $c1▄$c7█▀█$c1██$c7█▀█$c1██▄  $c2▄$c7█▀█$c2██$c7█▀█$c2██▄  $c4▄$c7█▀█$c4██$c7█▀█$c4██▄  $c5▄$c7█▀█$c5██$c7█▀█$c5██▄  $c6▄$c7█▀█$c6██$c7█▀█$c6██▄
-  $c1█$c7▄▄█$c1██$c7▄▄█$c1███  $c2█$c7▄▄█$c2██$c7▄▄█$c2███  $c4█$c7▄▄█$c4██$c7▄▄█$c4███  $c5█$c7▄▄█$c5██$c7▄▄█$c5███  $c6█$c7▄▄█$c6██$c7▄▄█$c6███
-  $c1████████████  $c2████████████  $c4████████████  $c5████████████  $c6████████████  
-  $c1██▀██▀▀██▀██  $c2██▀██▀▀██▀██  $c4██▀██▀▀██▀██  $c5██▀██▀▀██▀██  $c6██▀██▀▀██▀██  
-  $c1▀   ▀  ▀   ▀  $c2▀   ▀  ▀   ▀  $c4▀   ▀  ▀   ▀  $c5▀   ▀  ▀   ▀  $c6▀   ▀  ▀   ▀  
-"
-
-  set_color normal
-end
-
+# Shell exports
 set -x EDITOR $(which nvim)
 set -x GTK_IM_MODULE fcitx
 set -x QT_IM_MODULE fcitx
 set -x XMODIFIERS "@im=fcitx"
 
-function cat
-  bat --theme=gruvbox-dark $argv
-end
-
-function ls
-  eza --git --icons $argv
-end
+alias cat "bat --theme=gruvbox-dark"
+alias ls "eza --git --icons"
+alias ll "ls -l"
+alias la "ls -a"
+alias lla "ls -la"
+alias nv "nvim"
 
 function mdc
   pandoc $argv \
@@ -42,7 +20,6 @@ function mdc
     -V "numbersections:true" \
     -V "geometry:margin=30mm"
 end
-
 
 function mkcd
   mkdir $argv
@@ -53,20 +30,16 @@ function cdtemp
   cd $(mktemp -d)
 end
 
-alias ll "ls -l"
-alias la "ls -a"
-alias lla "ls -la"
-alias nv "nvim"
+# Vim keybindings
 
-function fish_hybrid_key_bindings --description \
-  "Vi-style bindings that inherit emacs-style bindings in insert mode"
+function fish_hybrid_key_bindings
+  # Emacs binding in insert mode
   fish_default_key_bindings -M insert
   fish_vi_key_bindings --no-erase
 end
 
 set -g fish_key_bindings fish_hybrid_key_bindings
 
-# Emulates vim's cursor shape behavior
 set fish_cursor_default block
 set fish_cursor_insert line
 set fish_cursor_replace_one underscore
