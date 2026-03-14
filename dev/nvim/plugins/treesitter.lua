2798acc169f42ed85ce764dd90b78ff4193e3931
Zap <67962871+ziap@users.noreply.github.com>
2026-03-14 11:44:06 +0700

    Update treesitter loading

--- dev/nvim/plugins/treesitter.lua
+++ dev/nvim/plugins/treesitter.lua
@@ -4,3 +4,23 @@ require'nvim-treesitter'.setup {
   highlight = { enable = true },
   indent = { enable = true },
 }
+
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function(args)
+    local buf, filetype = args.buf, args.match
+
+    local language = vim.treesitter.language.get_lang(filetype)
+    if not language then
+      return
+    end
+
+    -- check if parser exists and load it
+    if not vim.treesitter.language.add(language) then
+      return
+    end
+
+    -- enables syntax highlighting and other treesitter features
+    vim.treesitter.start(buf, language)
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
